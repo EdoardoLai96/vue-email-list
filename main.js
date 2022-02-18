6d8945f6967794f43381aea29805e3cddbe62b7b
Edoardo Lai <edoardolai96@gmail.com>
2022-02-18 09:57:32 +0100

    fix chiamate, le ho rese diverse tra di loro

--- main.js
+++ main.js
@@ -4,15 +4,18 @@ const app = new Vue({
         email_container: []
     },
     mounted(){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
 
+
+
+        for(let i=0; i < 10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        
         .then(function (response) {
-                for(let i=0; i < 10; i++){
                 // handle success
                 console.log(response.data.response);
                 app.email_container.push(response.data.response);
-            }
             })
+        }
     },
     methods: {
 
